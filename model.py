f5da8f0c3337a574c0e46ded7779b2f05b5f3586
P0v0n <pavandange@everymedia.world>
2026-01-30 17:53:41 +0530

    change

--- model.py
+++ model.py
@@ -1,6 +1,1 @@
-import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyADtnFwPAMwmNkXgg93FFBFU50xUOC3IAc")
-
-for m in genai.list_models():
-    print(m.name, m.supported_generation_methods)
